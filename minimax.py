ac3524cfa56853047fc2b4a527746906a6c48bb7
Katherine Varela <katherinevarela775@gmail.com>
2026-02-09 17:23:40 -0300

    Implementacion de movimientos

--- minimax.py
+++ minimax.py
@@ -1,3 +1,5 @@
+import random
+
 class Laberinto:
  def __init__(self, filas, columnas):
     self.filas = filas 
@@ -15,14 +17,48 @@ class Laberinto:
 
     return tablero
  def mostrar(self):
-    tablero_actual = self.generar_tablero()J
+    tablero_actual = self.generar_tablero()
     for fila in tablero_actual:
         print(" ".join(fila)) # Une los elementos con un espacio
    
     print("-" * 20) # Linea separadora de turnos
 
+ def calcular_distancia(self):
+    # Dist. Manhattan, le ayuda al gato a saber si se acerca al raton
+    return abs(self.gato_pos[0] - self.raton_pos[0]) + abs(self.gato_pos[1]- self.raton_pos[1])
 
- 
+ def mover_raton_azar(self):
+    # El raton se mueve al azar
+    direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)] # Arriba, Abajo, Izq., Der.
+
+    movimiento = random.choice(direcciones)
+
+    nueva_f = self.raton_pos[0] + movimiento[0]
+    nueva_c = self.raton_pos[1] + movimiento[1]
+
+    # Validacion1: que no salga del terreno de juego
+    if 0 <= nueva_f < self.filas and 0 <= nueva_c < self.columnas:
+      self.raton_pos = [nueva_f, nueva_c]
+
+ def mover_gato_simple(self):
+   # Aqui el gato intenta reducir distancia
+   direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+   mejor_movimiento = self.gato_pos
+   distancia_minima = float('inf')
+
+   for d in direcciones:
+      f = self.gato_pos[0] + d[0]
+      c = self.gato_pos[1] + d[1]
+
+      if 0 <= f < self.filas and 0 <= c < self.columnas:
+         # Se simula la distancia en caso de movernos ahi
+         dist = abs(f - self.raton_pos[0]) + abs(c - self.raton_pos[1])
+         if dist < distancia_minima:
+            distancia_minima = dist
+            mejor_movimiento = [f, c]
+
+   self.gato_pos =  mejor_movimiento
+
 
 
 
